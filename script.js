e21bbd27a78dcb16b94f04336777aadf6fe1b5d7
erin fox <erinkayfox@gmail.com>
2017-03-14 10:47:51 -0400

    add target

--- script.js
+++ script.js
@@ -3,8 +3,27 @@
 
 (function(){
 
-let cat = $('#cat');
-let target = $('#target');
+
+  let differentCats = [
+    ['crazyBabyCat','images/crazyBabyCat.png'],
+    ['orangeCat','images/orangeCat.png'],
+    ['sullyCat','images/sullyCat.png'],
+    ['whatCat','images/whatCat.png']
+  ];
+
+ let newCat = function(){
+    let $catImage= $('#cat_img');
+    $catImage.attr('src', differentCats[Math.floor(Math.random() * differentCats.length)][1]);
+  }
+
+  newCat();
+
+
+
+
+
+  let cat = $('#cat');
+  let target = $('#target');
 
 //MDN 2D Collison Detection
 
@@ -27,14 +46,19 @@ let $slowDown = $('#slow_btn');
 $startGame.click(function(){
 
 //http://stackoverflow.com/questions/16699330/make-a-div-move-across-the-screen
-      cat.animate({left: "+=1350"}, 5000);
+      cat.animate({left: "+=1350"}, 2000);
 
-      $slowDown.click(function(){
-        alert("working");
+      $slowDown.click(function() {
+        cat.stop();
+           $slowDown.click(function(){
+             cat.animate({left: "+=1350"}, 5000);
+
+           })
+
+
+
       })
-
-
-    });
+     })
 
 // catRight();
 
